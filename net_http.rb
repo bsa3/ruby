cf7b1588acf71178637f64699517b45d5762ee02
Brad <bsackerman3@gmail.com>
2016-12-13 05:32:15 -0800

    Update net_http.rb

--- net_http.rb
+++ net_http.rb
@@ -1,12 +1,15 @@
 require 'net/http'
 
-def post (URI, Username, Pass, content-type)  #strings like "application/json"
-  uri = URI(URI)
+def createfile(uri, username, pass, message, contenttype)  #strings like "application/json"
+  uri = URI(uri)
   req = Net::HTTP::Post.new(uri.to_s)
-  req.basic_auth(Username, Pass)
-  req.body = '{"body": "Test message"}'  # `to_json` can be used
-  req["content-type"] = content-type
+  req.basic_auth(username, pass)
+  req.body = '{"body": message}'  # `to_json` can be used
+  req["content-type"] = contenttype
   Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-    p response = http.request(req)
+    response = http.request(req)
+    response
   end
 end
+
+#createfile("https://github.com/repos/:owner/:repo/contents/xyz.txt", "user", "pass","message","application/json")
