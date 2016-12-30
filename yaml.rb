03c2226e79ebac246ff248412fb008e793d82ad6
BSA <bsackerman3@gmail.com>
2016-12-30 10:59:18 -0800

    Create yaml.rb

---
+++ yaml.rb
@@ -0,0 +1,19 @@
+#http://www.virtuouscode.com/2009/07/14/recursively-symbolize-keys/
+def symbolize_keys(hash)
+  hash.inject({}){|result, (key, value)|
+    new_key = case key
+              when String then key.to_sym
+              else key
+              end
+    new_value = case value
+                when Hash then symbolize_keys(value)
+                else value
+                end
+    result[new_key] = new_value
+    result
+  }
+end
+
+puts "Symbolized hash:"
+puts symbolize_keys(YAML.load(yaml)).inspect
+
