1aa25d3efbf611bcc485b5ab5470547505c0df81
BSA <bsackerman3@gmail.com>
2017-01-06 22:37:10 -0800

    Update default.rb

--- chef/attributes/default.rb
+++ chef/attributes/default.rb
@@ -13,7 +13,7 @@ case platform
 	when "centos", "redhat" #double checking platform
     default.cookbookname.featurename.FullFilePaths = [   "/var/log/x.log",
         "/var/log/y.log" ]
-    default.diskcleanup.dirwackerkey.FullDirPaths = [  "/tmp/Trash/"  ] #don't forget trailing slash
+    default.cookbookname.featurename.FullDirPaths = [  "/tmp/Trash/"  ] #don't forget trailing slash
 else
   puts "# Unknown platform in default attributes file!!!"
 end #case switch
